950dcaa9f27c384b4f4510786fe7130db950be64
talitast <talitatravassos.tt@gmail.com>
2019-04-14 12:27:53 -0300

    Updates

---
+++ precache-manifest.8a0e6e448174188e5fd5d5e34d1c25c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  },
+  {
+    "revision": "ba029aae85ee5166e1fa",
+    "url": "/random-quote-machine/static/js/main.5c3369f9.chunk.js"
+  },
+  {
+    "revision": "99539f2cb4aa66154b1b",
+    "url": "/random-quote-machine/static/js/2.9e1a3ddb.chunk.js"
+  },
+  {
+    "revision": "ba029aae85ee5166e1fa",
+    "url": "/random-quote-machine/static/css/main.8cbcceef.chunk.css"
+  },
+  {
+    "revision": "99539f2cb4aa66154b1b",
+    "url": "/random-quote-machine/static/css/2.954b546c.chunk.css"
+  },
+  {
+    "revision": "37af250160c69c0c1ba5ae67e9513d9d",
+    "url": "/random-quote-machine/index.html"
+  }
+];
