06534d5d37f27bf11c938139b5feaf81b59e6af9
talitast <talitatravassos.tt@gmail.com>
2019-04-14 11:32:22 -0300

    added footer and fix minor things

--- src/App.js
+++ src/App.js
@@ -122,10 +122,28 @@ const QuoteBox = (props) => {
           </button>
         </div>
       </div>
+      <Footer />
     </div>
   )
 }
 
+//import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="page-footer font-small">
+
+        <div className="footer-copyright text-center py-3 text-white">© 2019 Code by:
+          <a href="https://github.com/talitastravassos" className="text-white"> Talita S. T. </a>
+        </div>
+
+      </footer>
+    </div>
+  )
+}
+
+//export default Footer
 
 
 class App extends Component {
